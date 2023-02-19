dd0d03ab5508429831bb253010ebc77aff618cda
waheec19 <waheechowdhury80@gmail.com>
2023-02-18 19:02:02 -0600

    finalized testing

---
+++ Generated-README.md
@@ -0,0 +1,32 @@
+
+  # readmegen
+  
+  ![Github licence](https://img.shields.io/badge/license-MIT-blue)
+  ## Description
+  this project allows you to create quick readme files giving a brief description on projects
+  ## Table-of-Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## [Installation](#table-of-contents)
+  To install necessary dependencies, run the following command:
+    npm i
+  ## [Usage](#table-of-contents)
+  undefined
+  
+  ## [License](#table-of-contents)
+  The application is covered under the following license: [MIT](https://choosealicense.com/licenses/mit/)
+  ## [Contributing](#table-of-contents)
+  undefined
+  ## [Tests](#table-of-contents)
+  To run tests, run the following command:
+  
+    node index
+  ## [Questions](#table-of-contents)
+  If you have any question about the repo, open an issue or contact me directly at [Email: waheechowdhury80@gmail.com](mailto:waheechowdhury80@gmail.com).
+  You can find more of my work at [GitHub](https://github.com/waheec19).
+  
--- index.js
+++ index.js
@@ -1,100 +1,99 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const generateMarkdown = require("./utils/generateMarkdown");
-const api = require("./utils/api");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+let fileName = 'Generated-README.md';
 
 
 const questions = [
     {
-        type: "input",
-        name: "GitHubUsername",
-        message: "What is your GitHub Username?"
-    },
-    {
-        type: "input",
-        name: "ProjectTitle",
-        message: "What would you like your Project Title to be named?"
-    },
-    {
-        type: "input",
-        name: "ProjectDescription",
-        message: "Please write a short description of your project."
-    },
-    {
-        type: "input",
-        name: "Install",
-        message: "What are the steps required to install your project?"
-    },
-    {
-        type: "input",
-        name: "Usage",
-        message: "Please provide examples of how your project could be used."
-    },
-    {
-        type: "list",
-        name: "License",
-        message: "Choose what license you would like to use for your project.",
-        choices: [{
-            name: "GNU General Public License v3.0",
-            },
-            {
-            name: "MIT License"
-            },
-            {
-            name: "The Unlicense"
-            },
-            {
-            name: "Apache License 2.0"
-            }]
-    },
-    {
-        type: "input",
-        name: "Contributors",
-        message: "How many contributors will there be on your project?",
-        validate: validateContributors
-    },
-    {
-        type: "input",
-        name: "FAQ1",
-        message: "How do you initialize the project?"
-    }
-
+        type: 'input',
+        name: 'github',
+        message: 'What is your username?',
+        validate: githubInput => {
+          if (githubInput) {
+              return true;
+          } else {
+              console.log('Please enter your username!');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: emailInput => {
+          if (emailInput) {
+              return true;
+          } else {
+              console.log('Please enter your email!');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+          if (titleInput) {
+              return true;
+          } else {
+              console.log('Please enter your title!');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of the project you are working on.',
+        validate: descInput => {
+          if (descInput) {
+              return true;
+          } else {
+              console.log('Please enter the description!');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE-2.0', 'LGPL-3.0', 'BSL-1.0', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'What command should be run to install dependencies?',
+    
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+     
+      },
+     
+      
 ];
-
-function validateContributors(num)
-{
-   var reg = /^\d+$/;
-   return reg.test(num) || "Please enter a valid number";
-}
-
-function promptUser(){
-    return inquirer.prompt(questions)
-}
 
 
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err){
-        if (err){
-            console.error(err);
-        }
-    });
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Generating README.md file...')
+    );
 }
 
-async function init() {
-    console.log("Welcome to the README Generator!")
-    try {
-        const answers = await promptUser();
-        const user = await api.getUser(answers.GitHubUsername);
-        const readMe = generateMarkdown(answers, user);
-        writeToFile("GeneratedREADME.md", readMe);
-        console.log("**README file successfully created!**");
-        
-    }catch(err) {
-        console.log(err);
-        
-    }
-    
 
+function init() {
+  inquirer.prompt(questions)
+  .then((answers) => {
+    const readmeData = generateMarkdown(answers);
+    writeToFile(fileName, readmeData)
+  })
 }
+
 
 init();
